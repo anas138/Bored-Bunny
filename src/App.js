c0d52500dd7eb92e64a9846158964740ef77f035
anas138 <83574472+anas138@users.noreply.github.com>
2022-02-17 15:25:00 +0500

    added first page bunny

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     anas
+
+      <div className={"first-page"}>
+       <div className={"head-container"}>
+           <ul className={"header-list"}>
+               <li>HOME</li>
+               <li>WHAT IS BORED BUNNY</li>
+               <li>BENEFITS & UTILITIES</li>
+
+           </ul>
+           <div>
+               <img src={"https://www.boredbunny.io/images/logo.png"}/>
+           </div>
+           <ul className={"header-list"}>
+               <li>ROADMAP</li>
+               <li>THE TEAM</li>
+               <li>FAQ’S</li>
+               <button>Mint is Live</button>
+
+           </ul>
+
+       </div>
+
+        <div className={"paragraph"}>
+            <p>READY TO TAKE OVER THE METAVERSE AND THE NFT SPACE</p>
+            <h3> JOIN BORED BUNNY</h3>
+             <p>   Bored Bunny is your ticket into the largest and fastest growing NFT Project</p>
+            <button>Mint is Live</button>
+        </div>
+
+      </div>
+
+
+        <div className={"second-page"}>
+            <div className={"paragraph"}>
+                <p>Welcome To The Exclusive Bored Bunny</p>
+                <p>Join this ultra-exclusive NFT project featuring not only authentic art, but unmatched</p>
+                <p>utility!</p>
+                <button>VIEW ALL COLLECTION</button>
+            </div>
+        </div>
+
     </div>
   );
 }

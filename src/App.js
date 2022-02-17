e9b73ce5fe2226747bcc3fe215dfbf7bc69a9b8e
anas138 <83574472+anas138@users.noreply.github.com>
2022-02-17 18:10:45 +0500

    added ui

--- src/App.js
+++ src/App.js
@@ -6,44 +6,142 @@ function App() {
     <div className="App">
 
       <div className={"first-page"}>
-       <div className={"head-container"}>
-           <ul className={"header-list"}>
-               <li>HOME</li>
-               <li>WHAT IS BORED BUNNY</li>
-               <li>BENEFITS & UTILITIES</li>
+        <div className={"head-container"}>
+          <ul className={"header-list"}>
+            <li>HOME</li>
+            <li>WHAT IS BORED BUNNY</li>
+            <li>BENEFITS & UTILITIES</li>
 
-           </ul>
-           <div>
-               <img src={"https://www.boredbunny.io/images/logo.png"}/>
-           </div>
-           <ul className={"header-list"}>
-               <li>ROADMAP</li>
-               <li>THE TEAM</li>
-               <li>FAQ’S</li>
-               <button>Mint is Live</button>
+          </ul>
+          <div>
+            <img src={"https://www.boredbunny.io/images/logo.png"} />
+          </div>
+          <ul className={"header-list"}>
+            <li>ROADMAP</li>
+            <li>THE TEAM</li>
+            <li>FAQ’S</li>
+            <button>Mint is Live</button>
 
-           </ul>
+          </ul>
 
-       </div>
+        </div>
 
         <div className={"paragraph"}>
-            <p>READY TO TAKE OVER THE METAVERSE AND THE NFT SPACE</p>
-            <h3> JOIN BORED BUNNY</h3>
-             <p>   Bored Bunny is your ticket into the largest and fastest growing NFT Project</p>
-            <button>Mint is Live</button>
+          <p>READY TO TAKE OVER THE METAVERSE AND THE NFT SPACE</p>
+          <h3> JOIN BORED BUNNY</h3>
+          <p>   Bored Bunny is your ticket into the largest and fastest growing NFT Project</p>
+          <button>Mint is Live</button>
         </div>
 
       </div>
 
 
-        <div className={"second-page"}>
-            <div className={"paragraph"}>
-                <p>Welcome To The Exclusive Bored Bunny</p>
-                <p>Join this ultra-exclusive NFT project featuring not only authentic art, but unmatched</p>
-                <p>utility!</p>
-                <button>VIEW ALL COLLECTION</button>
-            </div>
+      <div className={"second-page"}>
+        <div className={"paragraph"}>
+          <p>Welcome To The Exclusive Bored Bunny</p>
+          <p>Join this ultra-exclusive NFT project featuring not only authentic art, but unmatched</p>
+          <p>utility!</p>
+          <button>VIEW ALL COLLECTION</button>
         </div>
+      </div>
+
+
+
+
+      <div className="third-page">
+        <div>
+          <img src="https://www.boredbunny.io/images/img_round2.png" />
+        </div>
+        <div className="thirdPage-text">
+          <div className="thirdPage-text-heading0ne">
+            <h3>WHAT IS<br />
+              BORED BUNNY?</h3>
+          </div>
+          <div className="thirdPage-text-headingtwo">
+            <h3>Bored Bunny is a collection of 4,999 unique 3D well-<br />
+              designed Bunnies united together to get on the Ethereum<br />
+              Blockchain</h3>
+          </div>
+          <div className="thirdPage-text-headingtwo">
+            <h3>Each Bored Bunny is unique and exclusive based on a<br />
+              hundred traits.<br />
+              The objective is to build the strongest community and<br />
+              project around NFTs.</h3>
+          </div>
+          <div className="thirdPage-text-headingtwo">
+            <h3>Entering into the Bored Bunny Community means joining a<br />
+              family full of investors and nft enthusiasts who believe in the<br />
+              future of Cryptocurrencies and the blockchain technology.</h3>
+          </div>
+          <div className="thirdPage-button">
+            <button>JOIN OUR DISCORD</button>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="fourth-Page">
+        <div>
+          <img src="https://www.boredbunny.io/images/img3.png" width="100%" />
+        </div>
+        <div>
+          <img src="https://www.boredbunny.io/images/img4.png" width="100%" />
+        </div>
+        <div>
+          <img src="https://www.boredbunny.io/images/mg5.png" width="100%" />
+        </div>
+        <div>
+          <img src="https://www.boredbunny.io/images/img6.png" width="100%" />
+        </div>
+        <div className="fourthPage-heading">
+          BORED BUNNY
+        </div>
+      </div>
+
+
+      <div className="fifth-page">
+        <div className="fifthPage-firstHeading">
+          <h3 className="fifthPage-firstHeading-sub">BENEFITS & UTILITIES</h3>
+          <h3>When you buy a Bored Bunny, you’re not simply buying a simple NFT, you are getting<br />
+            access to a community where benefits and utilities will increase with the time.</h3>
+        </div>
+        <div className="fiftPage-img">
+          <img src="https://www.boredbunny.io/images/img8.png" width="100%" />
+        </div>
+      </div>
+
+
+      <div className="sixth-page">
+        <div className="sixth-page-first-heading">
+          <h3>OUR SPECIAL<br />
+            LEGENDARY<br />
+            COLLECTION
+          </h3>
+          <h4>DISCOVER OUR 10 LEGENDARY BORED BUNNY - THEY ARE THE RAREST</h4>
+        </div>
+
+        <div className="sixthPage-imageContainer">
+          <img src="https://www.boredbunny.io/images/Legendary1.png" width="20%" height="100%" className="sixthPage-img" />
+          <img src="https://www.boredbunny.io/images/Legendary1.png" width="20%" height="100%" className="sixthPage-img" />
+          <img src="https://www.boredbunny.io/images/Legendary1.png" width="20%" height="100%" className="sixthPage-img" />
+          <img src="https://www.boredbunny.io/images/Legendary1.png" width="20%" height="100%" className="sixthPage-img" />
+          <img src="https://www.boredbunny.io/images/Legendary1.png" width="20%" height="100%" className="sixthPage-img" />
+        </div>
+      </div>
+
+
+      <div className="seventh-page">
+        <div className="seventhPage-heading">
+          <h3>ROAD MAP</h3>
+          <h4>Our plans moving forward into 2022.</h4>
+        </div>
+        <div>
+          <img src="https://www.boredbunny.io/images/img23.png" />
+        </div>
+        <div className="seventhPage-bottomImage">
+          <img src="https://www.boredbunny.io/images/img12.png" height="100%" width="100%"/>
+        </div>
+      </div>
 
     </div>
   );
